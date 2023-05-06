89083f5c90c6393d4d5ca2935c20ee5c75d4a664
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-06 05:41:40 +0200

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,18 +1,53 @@
 const fetch = require('node-fetch');
+const { createHttpLink } = require('apollo-link-http');
+const { setContext } = require('apollo-link-context');
+const { ApolloClient, InMemoryCache, gql } = require('@apollo/client');
 const cheerio = require('cheerio');
 
 async function redirect(req, res) {
   // Replace this with the domain of your WordPress site
-  const wordpressDomain = 'https://dailytrendings.info/';
-
-  // Get the requested path from the URL
-  const path = req.url;
+  const wordpressDomain = 'example.com';
 
   // Build the WordPress URL to fetch the meta tags
+  const path = req.url;
   const url = `https://${wordpressDomain}${path}`;
 
+  // Fetch the WordPress post ID from the GraphQL API
+  const httpLink = createHttpLink({ uri: `https://${wordpressDomain}/graphql` });
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.WP_GRAPHQL_AUTH_TOKEN}`,
+      }
+    };
+  });
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+  const query = gql`
+    query {
+      postBy(uri: "${path}") {
+        id
+        title
+        excerpt
+        featuredImage {
+          node {
+            sourceUrl(size: MEDIUM_LARGE)
+          }
+        }
+      }
+    }
+  `;
+  const { data } = await client.query({ query });
+  const { id, title, excerpt, featuredImage } = data.postBy;
+
+  // Build the WordPress URL with the post ID as query parameter
+  const redirectUrl = `https://${wordpressDomain}?p=${id}`;
+
   // Fetch the WordPress URL
-  const response = await fetch(url);
+  const response = await fetch(redirectUrl);
 
   if (response.status !== 200) {
     // If the WordPress URL doesn't exist, return a 404 error
@@ -21,13 +56,12 @@ async function redirect(req, res) {
     // If the WordPress URL exists, extract the meta tags from the HTML
     const html = await response.text();
     const $ = cheerio.load(html);
-    const title = $('meta[property="og:title"]').attr('content');
-    const description = $('meta[property="og:description"]').attr('content');
-    const image = $('meta[property="og:image"]').attr('content');
+    $('meta[property="og:title"]').attr('content', title);
+    $('meta[property="og:description"]').attr('content', excerpt);
+    $('meta[property="og:image"]').attr('content', featuredImage?.node?.sourceUrl);
 
-    // Redirect to the WordPress URL with the meta tags as query parameters
-    const redirectUrl = `https://${wordpressDomain}${path}?title=${encodeURIComponent(title)}&description=${encodeURIComponent(description)}&image=${encodeURIComponent(image)}`;
-    res.redirect(301, redirectUrl);
+    // Return the modified HTML
+    res.send($.html());
   }
 }
 
